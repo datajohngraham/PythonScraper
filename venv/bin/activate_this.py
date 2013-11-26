b993f42fde9cb1a9e6cac9b27d540df1d6501045
datajohngraham <johngraham415@gmail.com>
2013-11-26 11:28:53 -0800

    activate_this.py comments

--- venv/bin/activate_this.py
+++ venv/bin/activate_this.py
@@ -1,12 +1,19 @@
 """By using execfile(this_file, dict(__file__=this_file)) you will
 activate this virtualenv environment.
 
+######  documentation says  It is different from the import statement in that it does not use the module administration — it reads the file unconditionally and does not create a new module.
+##### what does it mean 'not create a new module'
+#####  in the execfile above, this_file means "activate_this.py"? 
+######   is the dict() method above referring to the dictionary where the module namespace is saved?  
+#######  __file__ is set the the full path and name of this 'activate_this.py' file?
+
 This can be used when you must use an existing Python interpreter, not
 the virtualenv bin/python
 """
 
 try:
     __file__
+
 except NameError:
     raise AssertionError(
         "You must run this like execfile('path/to/activate_this.py', dict(__file__='path/to/activate_this.py'))")
@@ -14,7 +21,9 @@ import sys
 import os
 
 old_os_path = os.environ['PATH']
+#######   get's the path name of the home directory
 os.environ['PATH'] = os.path.dirname(os.path.abspath(__file__)) + os.pathsep + old_os_path
+########      os.pathsep gives the way paths are separated in the OS,  for windows it's ;
 base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if sys.platform == 'win32':
     site_packages = os.path.join(base, 'Lib', 'site-packages')
